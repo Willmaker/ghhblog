cc342788fb9218f78c3a660e73ff00ee752614c8
ghh <gao_hh@163.com>
2021-11-14 21:11:50 +0800

    ghh blog

---
+++ article/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+
+from . import views
+app_name = 'article'
+
+urlpatterns = [
+    path('article-list/', views.article_list, name='article_list'),
+    # 文章详情
+    path('article-detail/<int:id>/', views.article_detail, name='article_detail'),
+    path('article-create/', views.article_create, name='article_create'),
+    # path('article-delete/<int:id>/', views.article_delete, name='article_delete'),
+    path(
+        'article-safe-delete/<int:id>/',
+        views.article_safe_delete,
+        name='article_safe_delete'
+    ),
+    # 更新文章
+    path('article-update/<int:id>/', views.article_update, name='article_update'),
+
+]
